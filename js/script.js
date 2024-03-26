3d6e77e438cf9b4ef88059953eacd3536d4811fc
amir mohammad firoozvand <firoozvand655@gmail.com>
2024-03-26 14:14:00 +0330

    end

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿let userNameInput = document.querySelector('.username')
+let passwordInput = document.querySelector('.password')
+let modal = document.querySelector('.modal')
+
+function dataValidation(event) {
+    event.preventDefault()
+    let userNameValue = userNameInput.value
+    let passwordValue = passwordInput.value
+
+    if (userNameValue.length < 12 || passwordValue.length < 8) {
+        // alert('Error')
+        modal.style.background = 'rgb(223, 28, 28)'
+        modal.innerHTML = 'Please enter the information correctly'
+        modal.style.display = 'inline'
+    } else {
+        modal.style.background = 'green'
+        modal.innerHTML = 'Login was successful'
+        modal.style.display = 'inline'
+    }
+
+    setTimeout(function () {
+        modal.style.display = 'none'
+    }, 7000)
+}
